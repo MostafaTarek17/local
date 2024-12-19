fb47beb181e0a8bf1ca572398f6baf1e5910e87f
Mostafa Tarek <mustafa.arram17@gmail.com>
2024-12-19 23:18:16 +0200

    another change

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 int main(){
-    cout << "Hello world!" << endl;
-    cout << "Hello C++!" << endl;
-    cout << "Hello Mostafa!" << endl;
+    int x,y;
+    cin >> x >> y;
+    cout << x << y;
 };
